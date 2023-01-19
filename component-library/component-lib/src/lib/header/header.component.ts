6db1ac064d9d2d2f7550a3ecd2f69000a40023af
kristopher-houston <kristopher.houston.cic.gc.ca>
2023-01-19 11:15:57 -0500

    decoupled lang switch from project translate files

--- component-library/component-lib/src/lib/header/header.component.ts
+++ component-library/component-lib/src/lib/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
 export const ENGLISH_BANNER_URL = 'https://www.canada.ca/etc/designs/canada/wet-boew/assets/sig-blk-en.svg';
--- component-library/component-lib/src/lib/language-switch/language-switch.component.ts
+++ component-library/component-lib/src/lib/language-switch/language-switch.component.ts
@@ -1,31 +1,62 @@
 import { isPlatformBrowser } from '@angular/common';
-import { Component, EventEmitter, HostListener, Inject, Input, OnInit, Output, PLATFORM_ID } from '@angular/core';
+import { Component, HostListener, Inject, Input, OnInit, PLATFORM_ID } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 import { LanguageSwitchButtonService } from './language-switch-button.service';
+
+export const LANGUAGE_SWITCH_TEXT_ENGLISH = 'Français';
+export const LANGUAGE_SWITCH_TEXT_ENGLISH_MOBILE = 'FR';
+export const LANGUAGE_SWITCH_ARIA_ENGLISH = 'Changer la langue à français'
+
+export const LANGUAGE_SWITCH_TEXT_FRENCH = 'English';
+export const LANGUAGE_SWITCH_TEXT_FRENCH_MOBILE = 'EN';
+export const LANGUAGE_SWITCH_ARIA_FRENCH = 'Switch language to English'
+
 
 @Component({
   selector: 'lib-language-switch',
   templateUrl: './language-switch.component.html',
-  styleUrls: ['./language-switch.component.scss']
 })
-export class LanguageSwitchComponent {
+export class LanguageSwitchComponent implements OnInit {
 
   @Input() id = '';
 
   isMobile = false;
 
+  text = '';
+  aria = '';
+
   constructor(
     @Inject(PLATFORM_ID) private platformId: object,
-    private langToggle: LanguageSwitchButtonService) { }
+    private langToggle: LanguageSwitchButtonService,
+    private translate: TranslateService) { }
 
-    /** Listens for screen resizes and sets mobile boolean */
-    @HostListener('window:resize', ['$event'])
-    handleResize(e: any) {
-      if (isPlatformBrowser(this.platformId)) {
-        this.isMobile = window.innerWidth <= 893;
-      }
+  /** Listens for screen resizes and sets mobile boolean */
+  @HostListener('window:resize', ['$event'])
+  handleResize(e: any) {
+    if (isPlatformBrowser(this.platformId)) {
+      this.isMobile = window.innerWidth <= 893;
     }
+  }
 
-    switch() {
-      this.langToggle.languageToggleClick();
-    } 
+  ngOnInit() {
+    let lang = this.translate.currentLang;
+    this.setText(lang);
+    this.translate.onLangChange.subscribe(newLang => {
+      this.setText(newLang.lang);      
+    });
+  }
+
+  switch() {
+    this.langToggle.languageToggleClick();
+  }
+
+  setText(lang: string) {
+    if (lang === 'en-US') {
+      this.isMobile ? (this.text = LANGUAGE_SWITCH_TEXT_ENGLISH_MOBILE) : (this.text = LANGUAGE_SWITCH_TEXT_ENGLISH);
+      this.aria = LANGUAGE_SWITCH_ARIA_ENGLISH;
+    } else {
+      this.isMobile ? (this.text = LANGUAGE_SWITCH_TEXT_FRENCH_MOBILE) : (this.text = LANGUAGE_SWITCH_TEXT_FRENCH);
+      this.aria = LANGUAGE_SWITCH_ARIA_FRENCH;
+    }
+  }
 }
