e6e02988d67f0a99c2882477ffb4505257f71e3e
Mahsa Ameri <mahsaameri@Mahsas-MBP.ht.home>
2023-01-19 11:17:20 -0500

    Try to add select to test

--- component-library/src/app/test-page/test-page.component.ts
+++ component-library/src/app/test-page/test-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { IDropdownInputConfig } from 'ircc-ds-angular-component-library';
 
 
 @Component({
@@ -7,12 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./test-page.component.scss']
 })
 export class TestPageComponent implements OnInit {
+  form = new FormGroup({});
+
+  selectConfig: IDropdownInputConfig = {
+    id: 'select-dropdown-test',
+    label: "Dropdown test",
+    formGroup: new FormGroup({}),
+    category: 'secondary',
+    options: [
+      {
+        text: "One",
+        value: 'First'
+      },
+      {
+        text: "Two",
+        value: 'Second'
+      }
+    ]
+  }
 
   constructor(
   ) { }
 
   ngOnInit() {
-
+    this.form.addControl(this.selectConfig.id, new FormControl('', [Validators.required]));
   }
 
 
