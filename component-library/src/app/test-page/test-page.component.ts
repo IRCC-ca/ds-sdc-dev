c6e9c74e5cfdd70aa829e0545d071a67e0987239
Muhammad Naseer Ahmad <114417946+Naseer-007@users.noreply.github.com>
2023-01-24 11:48:04 -0500

    test page content (will remove it before i create a PR)

--- component-library/src/app/test-page/test-page.component.ts
+++ component-library/src/app/test-page/test-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup } from '@angular/forms';
+import { IJLInputComponentConfig, InputTypes } from 'ircc-ds-angular-component-library';
+// import { IJLInputComponentConfig } from 'component-lib/src/public-api';
 
 @Component({
   selector: 'app-test-page',
@@ -7,6 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./test-page.component.scss']
 })
 export class TestPageComponent implements OnInit {
+
+  form: FormGroup = new FormGroup({});
+
+  inputConfig : IJLInputComponentConfig = {
+    id: 'test_input_1',
+    formGroup: this.form,
+    label: 'Password',
+    desc: "This is password description",
+    hint: 'enter your password',
+    type: InputTypes.password,
+    placeholder: 'Placeholder text',
+    error: true,
+    required: true,
+  }
 
   constructor(
   ) { }
