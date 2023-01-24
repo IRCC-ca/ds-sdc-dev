d8ce9c6831c016dce74a36e3da1984ef38b9e8a1
Muhammad Naseer Ahmad <114417946+Naseer-007@users.noreply.github.com>
2023-01-24 16:18:14 -0500

    Removed unused imports and comments

--- component-library/src/app/test-page/test-page.component.ts
+++ component-library/src/app/test-page/test-page.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
-import { IJLInputComponentConfig, InputTypes } from 'ircc-ds-angular-component-library';
-// import { IJLInputComponentConfig } from 'component-lib/src/public-api';
 
 @Component({
   selector: 'app-test-page',
