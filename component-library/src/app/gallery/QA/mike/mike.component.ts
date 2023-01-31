490034b9bc1cd8111d45413bb5498ccd362b9400
Michael.Jalowiecki <Michael.Jalowiecki@cic.gc.ca>
2023-01-31 15:53:39 -0500

    banner qa commit1

--- component-library/src/app/gallery/QA/mike/mike.component.ts
+++ component-library/src/app/gallery/QA/mike/mike.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LanguageSwitchService } from '@app/@shared/language-switch/language-switch.service';
+import { FormGroup, FormControl} from '@angular/forms';
+import { IAutoTestComponentConfig, IAutoTestConfigObject } from '../auto-tester/auto-tester.component';
 
 @Component({
   selector: 'app-mike',
@@ -8,10 +10,71 @@ import { LanguageSwitchService } from '@app/@shared/language-switch/language-swi
 })
 export class MikeComponent implements OnInit {
 
+  form = new FormGroup({});
+
+  testerConfig: IAutoTestConfigObject = {
+    checkboxes: [
+        {
+          id: 'autoConfig_checkbox_title',
+          formGroup: this.form,
+          label: 'title'
+        },
+        {
+          id: 'autoConfig_checkbox_desc',
+          formGroup: this.form,
+          label: 'description'
+        },
+        {
+          id: 'autoConfig_checkbox_title',
+          formGroup: this.form,
+          label: 'cta - [coming soon]'
+        }
+    ],
+    dropdowns: [
+      {
+        id: 'autoConfig_type',
+        label: 'Type',
+        formGroup: this.form,
+        options: [
+          {
+            text: 'Information'
+          },
+          {
+            text: 'Warning'
+          },
+          {
+            text: 'Critical'
+          },
+          {
+            text: 'Success'
+          },
+          {
+            text: 'Generic'
+          }
+        ]
+      }
+    ]
+  }
+
+  testComponentConfig: IAutoTestComponentConfig = {
+    id: 'mike_tester',
+    formGroup: this.form,
+    testFields: this.testerConfig
+  }
+
   constructor(private altLang: LanguageSwitchService) { }
 
   ngOnInit() {
-    this.altLang.setAltLangLink('QATesting-alt');
+    this.altLang.setAltLangLink('mike-alt');
+
+    this.testerConfig.dropdowns?.forEach(i => {
+      this.form.addControl(i.id, new FormControl());
+
+    });
+    this.testerConfig.checkboxes?.forEach(i => {
+      this.form.addControl(i.id, new FormControl());
+    });
+
   }
 
 }
