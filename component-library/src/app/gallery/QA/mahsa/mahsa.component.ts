4992dfea96a40bf721c7d629194eb8274c6fd421
Mahsa Ameri <mahsaameri@Mahsas-MBP.ht.home>
2023-01-31 14:06:16 -0500

    Added header and select to test page

--- component-library/src/app/gallery/QA/mahsa/mahsa.component.ts
+++ component-library/src/app/gallery/QA/mahsa/mahsa.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { LanguageSwitchService } from '@app/@shared/language-switch/language-switch.service';
+import { IDropdownInputConfig } from 'ircc-ds-angular-component-library';
 
 @Component({
   selector: 'app-mahsa',
@@ -8,10 +10,33 @@ import { LanguageSwitchService } from '@app/@shared/language-switch/language-swi
 })
 export class MahsaComponent implements OnInit {
 
+  form: FormGroup = new FormGroup({});
+  
+  selectConfig: IDropdownInputConfig = {
+    id: 'select-dropdown-test',
+    formGroup: this.form,
+    label: "Dropdown test",
+    category: 'secondary',
+    options: [
+      {
+        text: "One",
+        value: 'First'
+      },
+      {
+        text: "Two",
+        value: 'Second'
+      }
+    ],
+    required: true,
+    hint: "Hint Text",
+    desc: "Description text",
+  };
+
   constructor(private altLang: LanguageSwitchService) { }
 
   ngOnInit() {
-    this.altLang.setAltLangLink('QATesting-alt');
+    this.altLang.setAltLangLink('Mahsa-alt');
+    this.form.addControl(this.selectConfig.id, new FormControl());
   }
 
 }
