37c56f1a2f612a6ff15f507d654513c86d44f831
Michael.Jalowiecki <Michael.Jalowiecki@cic.gc.ca>
2023-02-01 16:19:20 -0500

    removing console logs

--- component-library/src/app/gallery/QA/mike/mike.component.ts
+++ component-library/src/app/gallery/QA/mike/mike.component.ts
@@ -101,10 +101,7 @@ export class MikeComponent implements OnInit {
       };
 
       for(let param in x){
-          console.log(param);
-          console.log(x[param]);
           updatedConfig = {...updatedConfig, [param] : x[param]}
-          console.log('updatedConfig: ', updatedConfig);
           this.qaBanner = updatedConfig;
 
       }
