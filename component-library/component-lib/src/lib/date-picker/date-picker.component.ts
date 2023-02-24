cfdac096ef2ce8f65cfabde0b27717e97842927e
kristopher-houston <kristopher.houston.cic.gc.ca>
2023-02-24 14:49:49 -0500

    several changes

--- component-library/component-lib/src/lib/date-picker/date-picker.component.ts
+++ component-library/component-lib/src/lib/date-picker/date-picker.component.ts
@@ -1,5 +1,49 @@
 import { Component, forwardRef, Input, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, NG_VALUE_ACCESSOR, FormControl } from '@angular/forms';
+import { TranslateService } from '@ngx-translate/core';
+import { DSSizes, IErrorPairs, StandAloneFunctions } from '../../public-api';
+import { DropdownType, IDropdownInputConfig } from '../dropdown-input/dropdown-input.component';
+
+export const DATE_PICKER_MONTHS_EN = [
+  'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'
+];
+
+export const DATE_PICKER_MONTHS_FR = [
+  'janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre'
+];
+
+export const DATE_PICKER_DAY_CONTROL_ID_EXTENSION = '_dayControl';
+export const DATE_PICKER_MONTH_CONTROL_ID_EXTENSION = '_monthControl';
+export const DATE_PICKER_YEAR_CONTROL_ID_EXTENSION = '_yearControl';
+
+
+
+export interface IDatePickerConfig {
+  id: string;
+  formGroup: FormGroup;
+  size?: keyof typeof DSSizes;
+  label?: string;
+  category?: keyof typeof DropdownType
+  required?: boolean;
+  hint?: string;
+  desc?: string;
+  errorMessages?: IDatePickerErrorMessages;
+}
+
+export interface IDatePickerErrorMessages {
+  general?: IErrorPairs[];
+  day?: IErrorPairs[];
+  month?: IErrorPairs[];
+  year?: IErrorPairs[];
+}
+
+export interface IDatePickerDropDownConfigs {
+  day: IDropdownInputConfig;
+  month: IDropdownInputConfig;
+  year: IDropdownInputConfig;
+}
+
+
 
 @Component({
   selector: 'lib-date-picker',
@@ -13,45 +57,91 @@ import { FormGroup, FormBuilder, Validators, NG_VALUE_ACCESSOR, FormControl } fr
   ]
 })
 export class DatePickerComponent implements OnInit {
-  @Input() title?: string;
-  @Input() dateForm: FormGroup = new FormGroup({});
+  @Input() config: IDatePickerConfig = {
+    id: '',
+    formGroup: new FormGroup({}),
+    category: 'secondary'
+  }
+
+  @Input() formGroup: FormGroup = new FormGroup({});
   @Input() id?: string;
+  @Input() size?: keyof typeof DSSizes;
+  @Input() label?: string;
+  @Input() category?: keyof typeof DropdownType
+  @Input() required?: boolean;
+  @Input() hint?: string;
+  @Input() desc?: string;
+  @Input() errorMessages?: IDatePickerErrorMessages;
+
+
   days: number[] = [];
   months: string[] = [];
-  years: number[] = [];
+
+  dropDownConfigs: IDatePickerDropDownConfigs = {
+    day: {
+      id: this.config.id + DATE_PICKER_DAY_CONTROL_ID_EXTENSION,
+      formGroup: this.config.formGroup,
+      label: ''
+    },
+    month : {
+      id: this.config.id + DATE_PICKER_MONTH_CONTROL_ID_EXTENSION,
+      formGroup: this.config.formGroup,
+      label: ''
+    },
+    year : {
+      id: this.config.id + DATE_PICKER_YEAR_CONTROL_ID_EXTENSION,
+      formGroup: this.config.formGroup,
+      label: ''
+    },
+  }
 
   //Get the current year for use in the year dropdown
   private currentYear = new Date().getFullYear();
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private translate: TranslateService,
+    public standAloneFunctions: StandAloneFunctions) { }
 
   ngOnInit() {
+    //set config from individual options, if present
+    if (this.formGroup) this.config.formGroup = this.formGroup;
+    if (this.id) this.config.id = this.id;
+    if (this.size) this.config.size = this.size;
+    if (this.label) this.config.label = this.label;
+    if (this.category) this.config.category = this.category;
+    if (this.required) this.config.required = this.required;
+    if (this.hint) this.config.hint = this.hint;
+    if (this.desc) this.config.desc = this.desc;
+    if (this.errorMessages) this.config.errorMessages = this.errorMessages;
+
     // Populate the months and years arrays
-    this.months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    this.setMonthsLanguage();
+    this.translate.onLangChange.subscribe(() => {
+      this.setMonthsLanguage();
+    });
     for (let i = 1900; i <= this.currentYear; i++) {
-      this.years.push(i);
+      this.dropDownConfigs.year.options.push();
     }
 
-    // Set up a form control for the month and year
-    this.dateForm.addControl((this.id + '_monthControl'), new FormControl('', Validators.required));
-    this.dateForm.addControl((this.id + '_yearControl'), new FormControl('', Validators.required));
-    this.dateForm.addControl((this.id + '_dayControl'), new FormControl('', Validators.required));
-
     // Populate the days array based on the selected month and year
-    this.dateForm.get((this.id + '_monthControl'))?.valueChanges.subscribe(month => {
+    this.config.formGroup.get((this.id + DATE_PICKER_MONTH_CONTROL_ID_EXTENSION))?.valueChanges.subscribe(month => {
       //add if statement here - the value of year can be empty, since it may not have been selected yet.
-      const numDays = this.updateDaysArray(month, this.dateForm.get((this.id + '_yearControl'))?.value);
+      const numDays = this.updateDaysArray(month, this.config.formGroup.get((this.id + '_yearControl'))?.value);
       console.log(month, numDays);
-      console.log(this.dateForm.get((this.id + '_yearControl'))?.value);
+      console.log(this.config.formGroup.get((this.id + DATE_PICKER_YEAR_CONTROL_ID_EXTENSION))?.value);
     });
-    this.dateForm.get((this.id + '_yearControl'))?.valueChanges.subscribe(year => {
-      const numDays = this.updateDaysArray(this.dateForm.get((this.id + '_monthControl'))?.value, year);
+    this.config.formGroup.get((this.id + DATE_PICKER_YEAR_CONTROL_ID_EXTENSION))?.valueChanges.subscribe(year => {
+      const numDays = this.updateDaysArray(this.config.formGroup.get((this.id + DATE_PICKER_MONTH_CONTROL_ID_EXTENSION))?.value, year);
     });
     if (this.days.length === 0) {
       for (let i = 1; i <= 31; i++) {
         this.days.push(i);
       }
     }
+  }
+
+  setMonthsLanguage() {
+    (this.translate.currentLang === 'en') || (this.translate.currentLang === 'en-US') ?
+      this.months = DATE_PICKER_MONTHS_EN : this.months = DATE_PICKER_MONTHS_FR;
   }
 
   /**
@@ -65,7 +155,7 @@ export class DatePickerComponent implements OnInit {
     for (let i = 1; i <= numDays; i++) {
       this.days.push(i);
     }
-    this.dateForm.get((this.id + '_dayControl'))?.setValue('');
+    this.config.formGroup.get((this.id + DATE_PICKER_DAY_CONTROL_ID_EXTENSION))?.setValue('');
   }
 
   /**
@@ -101,54 +191,41 @@ export class DatePickerComponent implements OnInit {
   }
 
   /**
-   * Switch function that grabs the month number based on the month name/lov
+   * Function that grabs the month number based on the month name/lov
    * @param month string representing the month
    * @returns number representing the month (Jan = 1, etc.)/ or 0, if no match found.
    */
   private getMonthNum(month: string) {
-      switch (month) {
-        case 'January':
-          return 1;
-        case 'February':
-          return 2;
-        case 'March':
-          return 3;
-        case 'April':
-          return 4;
-        case 'May':
-          return 5;
-        case 'June':
-          return 6;
-        case 'July':
-          return 7;
-        case 'August':
-          return 8;
-        case 'September':
-          return 9;
-        case 'October':
-          return 10;
-        case 'November':
-          return 11;
-        case 'December':
-          return 12;
-        default:
-          return 0;
+    return (this.months.findIndex(i => i === month) + 1);
+  }
+
+  //TODO: Getting errors requires some thought
+  getErrorAria(formGroup: FormGroup, id: string, errorMessages: IDatePickerErrorMessages) {
+    let returnError = '';
+    if (formGroup.get(id + DATE_PICKER_DAY_CONTROL_ID_EXTENSION)?.dirty && formGroup.get(id + DATE_PICKER_DAY_CONTROL_ID_EXTENSION)?.invalid && errorMessages.general) {
+      errorMessages?.general?.forEach(error => {
+        if (formGroup.get(id)?.errors?.[error.key]) {
+          (returnError === '') ? (returnError += this.translate.instant(error.errorLOV)) :
+            (returnError += (', ' + this.translate.instant(error.errorLOV)));
+        }
+      });
     }
+    return returnError;
   }
 
   writeValue(obj: any): void {
     if (obj) {
-      this.dateForm.setValue(obj, { emitEvent: false });
+      this.config.formGroup.setValue(obj, { emitEvent: false });
     }
   }
   registerOnChange(fn: any): void {
-    this.dateForm.valueChanges.subscribe(fn);
+    this.config.formGroup.valueChanges.subscribe(fn);
   }
   registerOnTouched(fn: any): void {
     this.onTouched = fn;
   }
   setDisabledState?(isDisabled: boolean): void {
-    isDisabled ? this.dateForm.disable() : this.dateForm.enable();
+    isDisabled ? this.config.formGroup.disable() : this.config.formGroup.enable();
   }
   private onTouched: () => void = () => { };
 }
