567ae412e9f6ce71f3b61266b3fa919bf239b82a
Mahsa Ameri <mahsaameri@Mahsas-MacBook-Pro.local>
2023-03-01 13:22:36 -0500

    Updated temp and component and added scss

--- component-library/component-lib/src/lib/progress-indicator/progress-indicator.component.ts
+++ component-library/component-lib/src/lib/progress-indicator/progress-indicator.component.ts
@@ -1,11 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+import { DSSizes } from "../../shared/constants/jl-components/jl-components.constants/jl-components.constants";
+export interface IProgressIndicatorConfig {
+  id: string,
+  stepNumber?: string,
+  stepTitle?: string,
+  size?: keyof typeof DSSizes
+}
 @Component({
   selector: 'lib-progress-indicator',
   templateUrl: './progress-indicator.component.html',
 })
 export class ProgressIndicatorComponent implements OnInit {
 
+  @Input() config: IProgressIndicatorConfig = {
+    id: '',
+  };
   constructor() { }
 
   ngOnInit(): void {
--- component-library/src/app/gallery/test-page/test-page.component.ts
+++ component-library/src/app/gallery/test-page/test-page.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { LanguageSwitchService } from '@app/@shared/language-switch/language-switch.service';
-import { IDatePickerConfig } from 'component-lib/src/public-api';
-import { IBannerConfig } from 'ircc-ds-angular-component-library';
-
+import { IProgressIndicatorConfig } from 'ircc-ds-angular-component-library';
 
 @Component({
   selector: 'app-test-page',
@@ -13,28 +11,16 @@ import { IBannerConfig } from 'ircc-ds-angular-component-library';
 export class TestPageComponent implements OnInit {
   form = new FormGroup({});
 
-  datePickerConfig: IDatePickerConfig = {
-    id: 'datePicker',
-    label: 'test label',
-    category: 'secondary',
-    formGroup: this.form,
-    hint: 'Test hint',
-    required: true,
-    desc: 'Test description',
-    // size: 'small'
+  progressIndicatorConfig: IProgressIndicatorConfig = {
+    id: 'progress-indicator',
+    stepNumber: "Step 1",
+    stepTitle: 'Step title'
   }
 
 
   constructor(private altLang: LanguageSwitchService) { }
 
-  ngOnInit() {
-    this.form.addControl((this.datePickerConfig.id + '_dayControl'), new FormControl());
-    this.form.addControl((this.datePickerConfig.id + '_monthControl'), new FormControl());
-    this.form.addControl((this.datePickerConfig.id + '_yearControl'), new FormControl());
-    console.log(this.form.controls, this.datePickerConfig.formGroup.controls);
-
-
-  }
+  ngOnInit() {}
 }
 
 
