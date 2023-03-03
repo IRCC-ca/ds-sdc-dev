66761ddc416ffa210cc5e1e7bee019bb4eadbeb5
Mahsa Ameri <mahsaameri@Mahsas-MBP.ht.home>
2023-03-03 09:13:06 -0500

    Added config to component and added to temp

--- component-library/component-lib/src/lib/progress-indicator/progress-indicator.component.ts
+++ component-library/component-lib/src/lib/progress-indicator/progress-indicator.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { DSSizes } from "../../shared/constants/jl-components/jl-components.constants/jl-components.constants";
+import { IProgressTagsConfig } from '../progress-tags/progress-tags.component';
 import { ITabConfig } from '../tabs/tabs.component';
 export interface IProgressIndicatorConfig {
   id: string,
@@ -21,6 +22,12 @@ export class ProgressIndicatorComponent implements OnInit {
     id: '',
     formGroup: new FormGroup({}),
   };
+
+  tagConfig: IProgressTagsConfig = {
+    id: this.config.id,
+    size: this.config.size
+  };
+  
   constructor() { }
 
   ngOnInit(): void {
