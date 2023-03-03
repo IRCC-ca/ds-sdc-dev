d7f3649df03e9efa4aebdce162e174a711da35df
Muhammad Naseer Ahmad <114417946+Naseer-007@users.noreply.github.com>
2023-03-03 16:37:46 -0500

    added styles for select component

--- component-library/component-lib/src/lib/select/select.component.ts
+++ component-library/component-lib/src/lib/select/select.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { DSSizes } from '../../shared/constants/jl-components/jl-components.constants/jl-components.constants';
 
 @Component({
   selector: 'lib-select',
   templateUrl: './select.component.html'
 })
 export class SelectComponent implements OnInit {
+
+  @Input() size? : keyof typeof DSSizes;
 
   constructor() { }
 
