45fe12f22cc06fcabdd766f4f120f2ea1aa329bc
kristopher-houston <kristopher.houston.cic.gc.ca>
2023-03-09 10:37:23 -0500

    added previous and next pages

---
+++ component-library/src/app/accessibility-demo/accessibility-demo-next-page/accessibility-demo-next-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-accessibility-demo-next-page',
+  templateUrl: './accessibility-demo-next-page.component.html',
+  styleUrls: ['./accessibility-demo-next-page.component.scss']
+})
+export class AccessibilityDemoNextPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ component-library/src/app/accessibility-demo/accessibility-demo-previous-page/accessibility-demo-previous-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-accessibility-demo-previous-page',
+  templateUrl: './accessibility-demo-previous-page.component.html',
+  styleUrls: ['./accessibility-demo-previous-page.component.scss']
+})
+export class AccessibilityDemoPreviousPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
