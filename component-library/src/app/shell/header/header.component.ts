5417b0d8d313fabdb7f6292768a6b36e0d5eb4d8
Michael.Jalowiecki <Michael.Jalowiecki@cic.gc.ca>
2023-03-15 13:33:20 -0400

    fixing header and adding hidden page navs

--- component-library/src/app/shell/header/header.component.ts
+++ component-library/src/app/shell/header/header.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
+
 export class HeaderComponent implements OnInit {
+
+  @Input() id: string = '';
 
   menuHidden = true;
 
