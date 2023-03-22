bcc057fd911282b76942d6ad9b35c89774caf800
Michael.Jalowiecki <Michael.Jalowiecki@cic.gc.ca>
2023-03-22 16:31:21 -0400

    color refactor

--- component-library/src/app/gallery/test-page/test-page.component.ts
+++ component-library/src/app/gallery/test-page/test-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { LanguageSwitchService } from '@app/@shared/language-switch/language-switch.service';
-import {  } from 'ircc-ds-angular-component-library';
+import { IIndicatorConfig } from 'ircc-ds-angular-component-library';
 
 @Component({
   selector: 'app-test-page',
@@ -9,7 +9,14 @@ import {  } from 'ircc-ds-angular-component-library';
   styleUrls: ['./test-page.component.scss'],
 })
 export class TestPageComponent implements OnInit {
-  form = new FormGroup({});
+
+  indiConf : IIndicatorConfig = {
+    type: 'text',
+    category: 'strong',
+    purpose: 'status',
+    status: 'critical',
+    label: 'Testing this'
+  }
 
   constructor(private altLang: LanguageSwitchService) {}
 
