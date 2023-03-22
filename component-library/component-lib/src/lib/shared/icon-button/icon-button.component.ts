344b1c102cd568c57f63ed965ca74dbd314998cd
Mahsa Ameri <mahsaameri@Mahsas-MBP.ht.home>
2023-03-22 16:11:46 -0400

    Updated path

--- component-library/component-lib/src/lib/shared/icon-button/icon-button.component.ts
+++ component-library/component-lib/src/lib/shared/icon-button/icon-button.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, Output, EventEmitter, OnInit} from '@angular/core';
-import { DSFullSizes } from '../../../shared/constants/jl-components/jl-components.constants/jl-components.constants';
+import { DSFullSizes } from '../../../public-api';
 export enum IconButtonCategories {
   primary = 'primary',
   critical = 'critical',
