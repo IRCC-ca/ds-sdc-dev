4637b2f6e0265230d4d0a075573a67edb1c93bec
Michael.Jalowiecki <Michael.Jalowiecki@cic.gc.ca>
2023-03-28 10:05:22 -0400

    updated for project/access

--- component-library/src/app/accessibility-demo/accessibility-demo-routing.ts
+++ component-library/src/app/accessibility-demo/accessibility-demo-routing.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccessibilityDemoIntroPageComponent } from './accessibility-demo-intro-page/accessibility-demo-intro-page.component';
 import { AccessibilityDemoNextPageComponent } from './accessibility-demo-next-page/accessibility-demo-next-page.component';
 import { AccessibilityDemoPreviousPageComponent } from './accessibility-demo-previous-page/accessibility-demo-previous-page.component';
 import { AccessibilityDemoComponent } from './accessibility-demo.component';
 
 const routes: Routes = [
   { path: '', component: AccessibilityDemoComponent },
+  { path: 'intro', component: AccessibilityDemoIntroPageComponent}, //English
+  { path: 'introduction', component: AccessibilityDemoIntroPageComponent}, //French
   { path: 'previous', component: AccessibilityDemoPreviousPageComponent }, //English
   { path: 'précédent', component: AccessibilityDemoPreviousPageComponent }, //French
   { path: 'next', component: AccessibilityDemoNextPageComponent }, //English
