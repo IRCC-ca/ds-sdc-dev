ec6a689c2ed3cdeca08c3eb3909fe162e53193d0
kristopher-houston <kristopher.houston.cic.gc.ca>
2023-04-05 16:44:23 -0400

    updated to work with ngOnChanges

--- component-library/component-lib/src/lib/shared/icon/icon.component.ts
+++ component-library/component-lib/src/lib/shared/icon/icon.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, KeyValueDiffer, KeyValueDiffers } from '@angular/core';
+import { Component, Input, KeyValueDiffer, KeyValueDiffers, SimpleChanges } from '@angular/core';
 
 export interface IIconConfig {
     ariaLabel?: string;
@@ -13,30 +13,40 @@ export class IconComponent {
     @Input() config: IIconConfig = {
     };
 
+    @Input() ariaLabel?: string;
+    @Input() FA_keywords?: string;
+
     differ: KeyValueDiffer<string, any>;
 
     constructor(private differs: KeyValueDiffers) {
       this.differ = this.differs.find({}).create();
     }
 
-    ngDoCheck() {
-      const change = this.differ.diff(this);
-        if (change) {
-          let cont = document.getElementById('ds-icon-container');
-            change.forEachChangedItem(item => {
-              if(item.key === 'config' && cont){
-                  if(!item.currentValue.ariaLabel){
-                    cont.innerHTML = `
-                    <span class='${item.currentValue.FA_keywords}' aria-hidden='true'></span>
-                    `
-                  } else {
-                    cont.innerHTML = `
-                    <span class='${item.currentValue.FA_keywords}' aria-label='${item.currentValue.ariaLabel}'></span>
-                    `
-                  }
-              }
-            });
-        }
+    ngOnChanges(changes: SimpleChanges) {
+      if (changes['config']) {
+        console.log('config changed', changes['config'].currentValue);
+      }
+      
     }
+
+    // ngDoCheck() {
+    //   const change = this.differ.diff(this);
+    //     if (change) {
+    //       let cont = document.getElementById('ds-icon-container');
+    //         change.forEachChangedItem(item => {
+    //           if(item.key === 'config' && cont){
+    //               if(!item.currentValue.ariaLabel){
+    //                 cont.innerHTML = `
+    //                 <span class='${item.currentValue.FA_keywords}' aria-hidden='true'></span>
+    //                 `
+    //               } else {
+    //                 cont.innerHTML = `
+    //                 <span class='${item.currentValue.FA_keywords}' aria-label='${item.currentValue.ariaLabel}'></span>
+    //                 `
+    //               }
+    //           }
+    //         });
+    //     }
+    // }
 
 }
