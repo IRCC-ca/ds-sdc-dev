c302c6af8409fe9635757c25747e2ea56a13ef97
Mahsa Ameri <mahsaameri@Mahsas-MBP.ht.home>
2023-04-13 14:09:05 -0400

    Updated by using forEach

--- component-library/src/app/gallery/QA/mahsa/mahsa.component.ts
+++ component-library/src/app/gallery/QA/mahsa/mahsa.component.ts
@@ -276,26 +276,14 @@ export class MahsaComponent implements OnInit {
     });
 
     this.form.valueChanges.subscribe((value) => {
-      let updatedConfig: ISelectConfig = {
-        id: this.SELECT_ID,
-        formGroup: this.form,
-        options: [{ text: 'Option 1' }, { text: 'Option 2' }]
-      };
-      for (const param in value) {
-        if (!value['label']) {
-          delete this.qaSelect.label;
-        }
-        if (!value['desc']) {
-          delete this.qaSelect.desc;
-        }
-        if (!value['hint']) {
-          delete this.qaSelect.hint;
-        }
+      Object.keys(value).forEach((param) => {
+        if (!value['label']) delete this.qaSelect.label;
+        if (!value['desc']) delete this.qaSelect.desc;
+        if (!value['hint']) delete this.qaSelect.hint;
         if (value[param]) {
-          updatedConfig = { ...updatedConfig, [param]: value[param] };
-          this.qaSelect = updatedConfig;
+          this.qaSelect = { ...this.qaSelect, [param]: value[param] };
         }
-      }
+      });   
     });
 
     this.tagForm.valueChanges.subscribe((value) => {
