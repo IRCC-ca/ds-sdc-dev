0a3b4f599d5bdea590278e8f0574250da99a1597
Mahsa Ameri <mahsaameri@Mahsas-MacBook-Pro.local>
2023-04-25 11:15:01 -0400

    Added for developers page

--- component-library/src/app/pages/for-developers/for-developers.component.ts
+++ component-library/src/app/pages/for-developers/for-developers.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { SideNavConfig } from '@app/components/side-nav/side-nav.config';
+import { ISideNavDataInterface } from '@app/components/side-nav/side-nav.model';
 import { TranslateService } from '@ngx-translate/core';
 import { SlugifyPipe } from 'src/app/share/pipe-slugify.pipe';
 import { LangSwitchService } from '../../share/lan-switch/lang-switch.service';
 import { TranslatedPageComponent } from '../translated-page-component';
-
+import {ClipboardModule} from '@angular/cdk/clipboard';
 @Component({
   selector: 'app-for-developers',
   templateUrl: './for-developers.component.html',
@@ -11,12 +13,23 @@ import { TranslatedPageComponent } from '../translated-page-component';
   providers: [SlugifyPipe]
 })
 export class ForDevelopersComponent implements OnInit, TranslatedPageComponent {
+  rightNavData: ISideNavDataInterface[];
+  rightNavDataRaw: string[] = [ // list of all right nav items
+    'Overview.DeveloperHeading',
+    'Developers.GetStartedHeading',
+    'Developers.UsageHeading',
+    'Developers.FontAwesomeHeading',
+    'Developers.ReleasesHeading',
+  ]
   altLangLink = 'forDesigners';
 
   constructor(
     private translate: TranslateService,
-    private lang: LangSwitchService
-  ) {}
+    private lang: LangSwitchService,
+    private navBarConfig: SideNavConfig
+  ) {
+    this.rightNavData = navBarConfig.getRightNavBarConfig(this.rightNavDataRaw);
+  }
 
   ngOnInit(): void {
     this.lang.setAltLangLink(this.altLangLink);
