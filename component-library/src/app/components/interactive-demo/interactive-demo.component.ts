4531ad41d9f3fc585edfcf881593fc7f4626f439
Muhammad Naseer Ahmad <114417946+Naseer-007@users.noreply.github.com>
2023-04-27 10:03:45 -0400

    Added enum for componentType and a comment describing the Input variable

--- component-library/src/app/components/interactive-demo/interactive-demo.component.ts
+++ component-library/src/app/components/interactive-demo/interactive-demo.component.ts
@@ -2,6 +2,11 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { IIconButtonIconConfig } from 'ircc-ds-angular-component-library';
 import { IAccordionContainerConfig } from '../accordion-panel/accordion-container.component';
 
+
+export enum ComponentType {
+  banner = 'banner',
+}
+
 @Component({
   selector: 'app-interactive-demo',
   templateUrl: './interactive-demo.component.html',
@@ -12,7 +17,14 @@ export class InteractiveDemoComponent implements OnInit {
     class: 'fa-light fa-chevron-down'
   };
 
-  @Input() componentType? : string;
+
+  /*
+   This variable is added to the class of component container,
+   it is being used to overrride background color for interactive
+   component specifically for Banner in the scss file, since 
+   banner has a white background and rest have a grey background.
+  */
+  @Input() componentType? : keyof typeof ComponentType;
 
   accordionConfig: IAccordionContainerConfig = {
     id: 'InteractiveDemoComponentAcccordion',
