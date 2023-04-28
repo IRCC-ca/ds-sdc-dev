06dfb0d3785e4ff496c7a7967f80cfd15b954be0
Muhammad Naseer Ahmad <114417946+Naseer-007@users.noreply.github.com>
2023-04-28 08:37:43 -0400

    Finished button-documentation (except for Layout toggle)

--- component-library/src/app/pages/button-documentation/button-documentation.component.ts
+++ component-library/src/app/pages/button-documentation/button-documentation.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { LangSwitchService } from '../../share/lan-switch/lang-switch.service';
+import { FormControl, FormGroup } from '@angular/forms';
+import { slugTitleURLConfig, slugTitleURLType } from '@app/components/title-slug-url/title-slug-url.component';
+import { IButtonConfig, ICheckBoxComponentConfig, IRadioInputComponentConfig, ITabNavConfig } from 'ircc-ds-angular-component-library';
 
 @Component({
   selector: 'app-button-documentation',
@@ -6,10 +11,228 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./button-documentation.component.scss']
 })
 export class ButtonDocumentationComponent implements OnInit {
+  altLangLink = 'buttonDocumentation';
 
-  constructor() { }
+  constructor(
+    private translate: TranslateService,
+    private lang: LangSwitchService
+  ) {}
 
-  ngOnInit(): void {
+  form_interactive_button = new FormGroup({});
+
+  interactiveDemoSlugTitleURLConfig: slugTitleURLConfig = {
+    type: slugTitleURLType.primary,
+    title: 'Interactive Demo'
+  };
+
+  buttonConfig: IButtonConfig = {
+    id: 'button',
+    disabled: false,
+    icon: ''
+    
+  };
+
+  checkboxes : ICheckBoxComponentConfig[] = [
+    {
+      id: 'showSelectToggle',
+      formGroup: this.form_interactive_button,
+      label:'State',
+      size: 'small',
+      inlineLabel: 'Disabled'
+    }
+  ]
+
+  toggles: IRadioInputComponentConfig[] = [
+    {
+      id: 'showSizeToggle',
+      formGroup: this.form_interactive_button,
+      size: 'small',
+      label: 'Size',
+      options: [
+        {
+          text: 'Small'
+        },
+        {
+          text: 'Large'
+        }
+      ]
+    },
+    {
+      id: 'showCriticalToggle',
+      formGroup: this.form_interactive_button,
+      label: 'Critical',
+      size: 'small',
+      options: [
+        {
+          text: 'True'
+        },
+        {
+          text: 'False'
+        }
+      ]
+    },
+    // {
+    //   id: 'showLayoutToggle',
+    //   formGroup: this.form_interactive_button,
+    //   label: 'Layout',
+    //   size: 'small',
+    //   options: [
+    //     {
+    //       text: 'Fluid'
+    //     },
+    //     {
+    //       text: 'Fixed'
+    //     }
+    //   ]
+    // },
+    {
+      id: 'showIconToggle',
+      formGroup: this.form_interactive_button,
+      label: 'Icon',
+      size: 'small',
+      options: [
+        {
+          text: 'None'
+        },
+        {
+          text: 'Leading'
+        },
+        {
+          text: 'Trailing'
+        }
+      ]
+    },
+  ];
+
+  demoTabsConfig: ITabNavConfig = {
+    id: 'demoTabs',
+    size: "small",
+    tab: [
+      {
+        id: 'primary',
+        title: 'Primary'
+      },
+      {
+        id: 'secondary',
+        title: 'Secondary'
+      },
+      {
+        id: 'plain',
+        title: 'Plain'
+      }
+    ]
+  };
+
+  handleSizeToggle(value: any) {
+    this.buttonConfig.size = value['showSizeToggle'].toLowerCase();
+    console.log('Button config size', this.buttonConfig.size)
   }
 
+  handleIconToggle(value: any) {
+    console.log('handleIconToggle', value)
+    this.buttonConfig.icon ='fa-regular fa-igloo'
+    if (value['showIconToggle'] === 'Leading') {
+      this.buttonConfig.iconDirection='right'
+    } 
+    else if (value['showIconToggle'] === 'Trailing') {
+      this.buttonConfig.iconDirection='left'
+    }
+    else {
+      this.buttonConfig.icon =''
+    }
+  }
+
+  handleCriticalToggle(value: any) {
+    if (value['showCriticalToggle'] === 'True') {
+      this.buttonConfig.color = 'critical';
+    } else {
+      this.buttonConfig.color = 'CTA';
+    }
+  }
+
+  handleSelectCheckbox(value : any) {
+    if (value['showSelectToggle']) {
+      this.buttonConfig.disabled = true;
+    } else {
+      this.buttonConfig.disabled = false;
+    }
+  }
+
+  setButtonCategory(value: any) {
+    if (value === 'primary') {
+      this.buttonConfig.category = 'primary';
+    } else if (value === 'secondary') {
+      this.buttonConfig.category = 'secondary';
+    } else if (value === 'plain') {
+      this.buttonConfig.category = 'plain';
+    } 
+  }
+
+  assignToggleDefaultValues(toggleID : string, toggle : IRadioInputComponentConfig) {
+    if (toggle.options) {
+      switch(toggleID) {
+        case 'showIconToggle':
+          this.form_interactive_button.addControl(toggle.id,new FormControl(toggle.options[0].text));
+          break;
+        case 'showSizeToggle': 
+          this.form_interactive_button.addControl(toggle.id,new FormControl(toggle.options[1].text));
+          break;
+        case 'showCriticalToggle':
+          this.form_interactive_button.addControl(toggle.id,new FormControl(toggle.options[1].text));
+          break;
+        default: {
+          console.log("Default")
+          this.form_interactive_button.addControl(toggle.id,new FormControl(toggle.options[1].text));
+        }
+      }
+    }
+  }
+
+  track_toggles = {
+    showSizeToggle: 'Large',
+    showIconToggle: 'None',
+    showCriticalToggle: 'False',
+    showSelectToggle: false
+  };
+
+  toggle_function = {
+    showSizeToggle: this.handleSizeToggle,
+    showIconToggle: this.handleIconToggle,
+    showCriticalToggle: this.handleCriticalToggle,
+    showSelectToggle: this.handleSelectCheckbox
+  };
+
+  ngOnInit(): void {
+
+    this.lang.setAltLangLink(this.altLangLink);
+    
+    this.checkboxes.forEach((checkbox) => {
+      if (checkbox) {
+        this.form_interactive_button.addControl(
+          checkbox.id,
+          new FormControl()
+        );
+      }
+    });
+
+    this.toggles.forEach((toggle) => {
+      if (toggle.options) {
+        this.assignToggleDefaultValues(toggle.id, toggle)
+      }
+    });
+
+    this.form_interactive_button.valueChanges.subscribe((value: any) => {
+      console.log("------------value------------", value)
+      for (const param in value) {
+        console.log('Param', param)
+        console.log(`------>this.track_toggles[param]: ${this.track_toggles[param]}    value[param]", ${value[param]}`)
+        if (this.track_toggles[param] === value[param]) continue;
+        // console.log("Value",value)
+        // console.log('Param', param)
+        // // console.log()
+        this.track_toggles[param] = value[param];
+        this.toggle_function[param].apply(this, [value]);
+      }
+    });
+  }
 }
