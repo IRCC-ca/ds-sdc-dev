b8e679bf16f795afeb7135faf9f47871b5858c37
alexandreGrenierIRCC <Alexandre.Grenier@cic.gc.ca>
2023-04-28 11:02:57 -0400

    Changes

--- deploy-qa-to-s3.js
+++ deploy-qa-to-s3.js
@@ -62,7 +62,7 @@ const params = {
 (async () => {
   let branhcName = await getBranch();
   if (!branhcName.startsWith("qa")) {
-    console.log("\x1b[34m%s\x1b[0m", "Not sending branch to AWS");
+    console.log("\x1b[34m%s\x1b[0m", "Not sending branch to QA");
     return;
   }
 
