ec8de41dc68760a70014c776f21ea6a77635fa0c
alexandreGrenierIRCC <Alexandre.Grenier@cic.gc.ca>
2023-04-27 22:06:57 -0400

    Initial Commit

--- deploy-qa-to-s3.js
+++ deploy-qa-to-s3.js
@@ -11,7 +11,7 @@ const getBranch = () =>
     return exec("git rev-parse --abbrev-ref HEAD", (err, stdout, stderr) => {
       if (err) reject(`getBranch Error: ${err}`);
       else if (typeof stdout === "string")
-        resolve(stdout.trim().replace("/", "-"));
+        resolve(stdout.trim().replace("/", "-").toLowerCase());
     });
   });
 
