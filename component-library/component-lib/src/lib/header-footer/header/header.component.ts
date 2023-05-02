87ce8ac836f0c7bc6f88bf29d2160610561e9d4a
Michael.Jalowiecki <Michael.Jalowiecki@cic.gc.ca>
2023-05-02 14:18:01 -0400

    compodoc setup and comments

--- component-library/component-lib/src/lib/header-footer/header/header.component.ts
+++ component-library/component-lib/src/lib/header-footer/header/header.component.ts
@@ -5,13 +5,10 @@ export const ENGLISH_BANNER_URL =
   'https://www.canada.ca/etc/designs/canada/wet-boew/assets/sig-blk-en.svg';
 export const FRENCH_BANNER_URL =
   'https://www.canada.ca/etc/designs/canada/wet-boew/assets/sig-blk-fr.svg';
-
 export const HEADER_IMG_LINK_EN = 'https://www.canada.ca/en.html';
 export const HEADER_IMG_LINK_FR = 'https://www.canada.ca/fr.html';
-
 export const HEADER_LINK_ATL_EN = 'Government of Canada logo';
 export const HEADER_LINK_ALT_FR = 'Gouvernement du Canada logo';
-
 export const CANADA_LOGO_ARIA_LABEL_ENGLISH =
   'Symbol of the Government of Canada';
 export const CANADA_LOGO_ARIA_LABEL_FRENCH =
@@ -22,8 +19,10 @@ export const CANADA_LOGO_ARIA_LABEL_FRENCH =
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+  /**
+   * This is the ID of the header component. Will be applied as the ID of the header Element within the custom element.
+   */
   @Input() id = '';
-
   imageURL = '';
   aria = '';
   govCanadaLink = '';
@@ -31,13 +30,23 @@ export class HeaderComponent {
 
   constructor(private translate: TranslateService) {}
 
+  /**
+  * ngOnInit checks for current url Language and subscribes to changes. Appropriate translation will be pulled as a result.
+  */
   ngOnInit() {
+    /**
+     * testing comment inside ngOnInit()
+     */
     this.setLang(this.translate.currentLang);
     this.translate.onLangChange.subscribe((change) => {
       this.setLang(change.lang);
     });
   }
 
+  /**
+  * setLang(lang: string) accepts and string value which currently needs to be 'en' or 'en-US' to trigger English translations otherwise
+  * french translations will be triggered.
+  */
   setLang(lang: string) {
     if (lang === 'en' || lang === 'en-US') {
       this.imageURL = ENGLISH_BANNER_URL;
