0b27dae5eeced2002c210e9bbb4627e08f4ff576
Bobby Brice <bobbybrice@gmail.com>
2023-05-04 13:25:09 -0400

    begin template and banner config

--- component-library/src/app/pages/request-form/request-form.component.ts
+++ component-library/src/app/pages/request-form/request-form.component.ts
@@ -1,12 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { slugTitleURLConfig, slugTitleURLType, slugAnchorType } from '@app/components/title-slug-url/title-slug-url.component';
+import { LangSwitchService } from '@app/share/lan-switch/lang-switch.service';
+import { SlugifyPipe } from '@app/share/pipe-slugify.pipe';
+import { TranslateService } from '@app/share/templates/parent-template.module';
+import { IBannerConfig } from 'component-lib/src/lib/banner-component/banner/banner.component';
 
 @Component({
   selector: 'app-request-form',
   templateUrl: './request-form.component.html',
-  styleUrls: ['./request-form.component.scss']
+  styleUrls: ['./request-form.component.scss'],
+  providers: [SlugifyPipe]
 })
 export class RequestFormComponent implements OnInit {
-  constructor() {}
+  altLangLink = 'requestForm';
 
-  ngOnInit(): void {}
+  requestFormTitleSlugConfig: slugTitleURLConfig = {
+    type: slugTitleURLType.primary,
+    title: 'RequestForm.Title',
+    anchorType: slugAnchorType.primary
+  };
+
+  bannerConfig: IBannerConfig = {
+    id: 'request-form-banner',
+    content: 'Some content',
+    type: 'generic',
+    rounded: false,
+    dismissible: false,
+    size: 'small',
+    cta: []
+  }
+  constructor(private translate: TranslateService,
+    private lang: LangSwitchService) { }
+
+  ngOnInit(): void {
+    this.lang.setAltLangLink(this.altLangLink);
+
+  }
 }
