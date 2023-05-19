57c2672b3f14e0623f5b5139485e42e1414ac692
alexandreGrenierIRCC <Alexandre.Grenier@cic.gc.ca>
2023-05-19 10:58:50 -0400

    Changes

--- component-library/src/app/pages/overview/overview.component.ts
+++ component-library/src/app/pages/overview/overview.component.ts
@@ -3,6 +3,7 @@ import { TranslateService } from '@ngx-translate/core';
 import { SlugifyPipe } from '../../share/pipe-slugify.pipe';
 import { LangSwitchService } from '../../share/lan-switch/lang-switch.service';
 import { TranslatedPageComponent } from '../translated-page-component';
+import { HttpClient } from '@angular/common/http';
 
 import {
   slugTitleURLType,
@@ -19,29 +20,36 @@ import {
 export class OverviewComponent implements OnInit, TranslatedPageComponent {
   currentLanguage: string = '';
   altLangLink = 'overview'; // ROUTE translation path
-  slugTitleURLType = slugTitleURLType;
-  anchorType = slugAnchorType;
 
-  overViewSlugTitleURLConfig: slugTitleURLConfig = {
-    type: slugTitleURLType.primary,
-    title: 'Overview.Heading',
-    anchorType: slugAnchorType.primary
-  };
-  contactSlugTitleURLConfig: slugTitleURLConfig = {
-    type: slugTitleURLType.primary,
-    title: 'Overview.Contact',
-    anchorType: slugAnchorType.primary
-  };
+  headers = {};
+  body = { title: 'Angular POST Request Example' };
+  url = 'https://3zlx7xw3hfrthsatjdlyqkmq6q0ypbiu.lambda-url.us-east-2.on.aws/';
 
   constructor(
     private translate: TranslateService,
     private lang: LangSwitchService,
-    private slugify: SlugifyPipe
+    private slugify: SlugifyPipe,
+    private http: HttpClient
   ) {
     this.currentLanguage = translate.currentLang;
   }
 
-  ngOnInit(): void {
+  async ngOnInit() {
     this.lang.setAltLangLink(this.altLangLink);
+
+    console.log(await this.sendRequest());
+  }
+
+  async sendRequest() {
+    return new Promise((resolve, reject) => {
+      this.http.post<any>(this.url, this.body).subscribe({
+        next: (data) => {
+          resolve(data.body);
+        },
+        error: (error) => {
+          reject(`There was an error!: ${error}`);
+        }
+      });
+    });
   }
 }
