8caa4328595bf4dbbbab53620ff8bab1cc176de9
kristopher-houston <109545114+kristopher-houston@users.noreply.github.com>
2023-05-24 16:41:52 -0400

    updated the input component and lable scss to stop enter and outside clicks from triggering button

--- component-library/component-lib/src/lib/shared/label/label.component.ts
+++ component-library/component-lib/src/lib/shared/label/label.component.ts
@@ -71,7 +71,9 @@ export class LabelComponent implements OnInit {
    * Output the button press 
    * @param id of the button being pressed (same as component ID)
    */
-  iconButtonClick() {
+  iconButtonClick(event: Event) {
+    console.log(event);
+    event.stopPropagation();
     this.labelButton.buttonPress(this.config.parentID);
   }
 
