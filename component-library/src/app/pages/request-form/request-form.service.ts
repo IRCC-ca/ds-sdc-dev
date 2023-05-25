bfaadeb87ac3f0f915aae6069ac5d6b2597e217f
Bobby Brice <bobbybrice@gmail.com>
2023-05-25 11:34:32 -0400

    remove ses package and remove unused imports

--- component-library/src/app/pages/request-form/request-form.service.ts
+++ component-library/src/app/pages/request-form/request-form.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 export interface IRequestFormDataInterface {
   ['radio-request-type']?: string;
   ['request-title-text-area']?: string;
