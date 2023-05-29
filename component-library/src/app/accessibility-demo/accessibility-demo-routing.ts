967dc5a4aeffb2cfa5d8e59de7639f7936a07dce
Michael.Jalowiecki <Michael.Jalowiecki@cic.gc.ca>
2023-05-29 15:27:01 -0400

    removing redunancies cont

--- component-library/src/app/accessibility-demo/accessibility-demo-routing.ts
+++ component-library/src/app/accessibility-demo/accessibility-demo-routing.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AccessibilityDemoIntroPageComponent } from './accessibility-demo-intro-page/accessibility-demo-intro-page.component';
 import { AccessibilityDemoNextPageComponent } from './accessibility-demo-next-page/accessibility-demo-next-page.component';
 import { AccessibilityDemoPreviousPageComponent } from './accessibility-demo-previous-page/accessibility-demo-previous-page.component';
 import { AccessibilityDemoComponent } from './accessibility-demo.component';
-import { marker } from '@biesbjerg/ngx-translate-extract-marker';
 
 const routes: Routes = [
   { path: '', component: AccessibilityDemoComponent, title:'TITLES.PersonalInformation'},
