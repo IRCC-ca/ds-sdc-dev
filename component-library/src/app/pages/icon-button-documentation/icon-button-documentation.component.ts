000d5dc4a59c31c1c843ef22f0f39cf0b376b2a1
Bobby Brice <bobbybrice@gmail.com>
2023-06-15 11:18:53 -0400

    lint

--- component-library/src/app/pages/icon-button-documentation/icon-button-documentation.component.ts
+++ component-library/src/app/pages/icon-button-documentation/icon-button-documentation.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { slugTitleURLConfig, slugAnchorType } from '@app/components/title-slug-url/title-slug-url.component';
+import {
+  slugTitleURLConfig,
+  slugAnchorType
+} from '@app/components/title-slug-url/title-slug-url.component';
 import { docPageheadingConfig } from '@app/share/documentation-page-headings';
 import { LangSwitchService } from '@app/share/lan-switch/lang-switch.service';
 import { TranslateService } from '@ngx-translate/core';
-
 
 @Component({
   selector: 'app-icon-button-documentation',
@@ -14,7 +16,7 @@ export class IconButtonDocumentationComponent implements OnInit {
   headingConfig = docPageheadingConfig;
   currentLanguage: string = '';
   altLangLink = 'iconButtonDocumentation';
-  
+
   pageTitleSlugConfig: slugTitleURLConfig = {
     title: 'IconButtonDocumentation.Title',
     heading: 'h1',
@@ -30,5 +32,4 @@ export class IconButtonDocumentationComponent implements OnInit {
   ngOnInit() {
     this.lang.setAltLangLink(this.altLangLink);
   }
-
 }
