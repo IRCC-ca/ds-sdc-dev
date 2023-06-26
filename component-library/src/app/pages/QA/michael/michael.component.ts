94d2756ac17a3eebaa05ea6ff87f812db86bd921
Michael Xi <111757720+xingruoxicic@users.noreply.github.com>
2023-06-26 13:14:01 -0700

    Indicator demo: fix form glitch

--- component-library/src/app/pages/QA/michael/michael.component.ts
+++ component-library/src/app/pages/QA/michael/michael.component.ts
@@ -578,13 +578,14 @@ export class MichaelComponent implements OnInit {
           };
         }
       }
+      this.indicatorConfig = updatedConfig;
+
       // Update form label field
       if (this.form_2.get('label')?.value !== updatedConfig.label) {
         this.form_2.patchValue({
           label: updatedConfig.label
         });
       }
-      this.indicatorConfig = updatedConfig;
     });
     this.form_2.patchValue({
       category: 'strong',
