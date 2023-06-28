fb3c68abfb2a130f87270df370b81229c7a37f3d
Michael Xi <111757720+xingruoxicic@users.noreply.github.com>
2023-06-28 10:18:47 -0700

    PR fixes round 2

--- component-library/src/app/components/info-text-small/info-text-small.component.ts
+++ component-library/src/app/components/info-text-small/info-text-small.component.ts
@@ -14,7 +14,8 @@ import { IIconConfig } from 'dist/ircc-ds-angular-component-library/lib/shared/i
 export class InfoTextSmallComponent implements OnInit, TranslatedPageComponent {
   circleInfoIcon: IIconConfig = {
     FA_keywords: 'fa-light fa-circle-info',
-    ariaLabel: ''
+    ariaLabel: '',
+    size: 'small'
   };
 
   altLangLink = 'infoTextSmall';
