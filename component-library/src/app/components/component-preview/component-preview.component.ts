3a27d96d5c8aa3c436b7185c85b48bec1b7fb74a
Michael Xi <111757720+xingruoxicic@users.noreply.github.com>
2023-08-10 13:55:35 -0700

    component preview: refactor to support multi stacked up together, refer design to https://www.figma.com/file/TPwx1HcbXRCZeIDePI4Y4N/Documentation-Site?type=design&node-id=524-15609&mode=dev

--- component-library/src/app/components/component-preview/component-preview.component.ts
+++ component-library/src/app/components/component-preview/component-preview.component.ts
@@ -15,9 +15,17 @@ export class ComponentPreviewComponent implements OnInit {
   @Input() copyStyle?: string;
   @Input() backgroundColor?: keyof typeof BackgroundColor;
   @Input() minWidth: number = 0;
+  @Input() multiConfig?: {
+    multi: boolean;
+    position: 'first' | 'middle' | 'last';
+  };
+
+  positionClass?: string;
 
   ngOnInit() {
     if (!this.backgroundColor) this.backgroundColor = BackgroundColor.gray;
+    if (this.multiConfig && this.multiConfig.multi)
+      this.positionClass = 'multi-' + this.multiConfig.position;
 
     this.backgroundColor === BackgroundColor.white
       ? BackgroundColor.white
