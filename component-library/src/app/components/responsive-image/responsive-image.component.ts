e2f64a6c46bacfd0c1e6ed93a571eb6a6205b93a
Bobby Brice <bobbybrice@gmail.com>
2023-09-12 14:58:24 -0400

    refactor to allow flexible breakpoints

--- component-library/src/app/components/responsive-image/responsive-image.component.ts
+++ component-library/src/app/components/responsive-image/responsive-image.component.ts
@@ -1,30 +1,73 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  Renderer2,
+  ElementRef,
+  AfterViewInit,
+  ViewChild
+} from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
 export interface IResponsiveImageComponentConfig {
   id: string;
-  altText: string;
+  breakpoints: {
+    maxWidth: number;
+    src: string;
+  }[];
   defaultSrc: string;
-  desktopSrc: string;
-  mobileSrc: string;
-  maxWidth: number;
-  lazyLoad?: boolean | null;
+  altText: string;
+  lazyLoad?: boolean;
 }
+
 @Component({
   selector: 'app-responsive-image',
   templateUrl: './responsive-image.component.html',
   styleUrls: ['./responsive-image.component.scss']
 })
-export class ResponsiveImageComponent {
+export class ResponsiveImageComponent implements AfterViewInit {
+  @ViewChild('image', { static: true })
+  image!: ElementRef<HTMLImageElement>;
   @Input() config: IResponsiveImageComponentConfig = {
     id: '',
+    breakpoints: [{ maxWidth: 0, src: '' }],
     altText: '',
     defaultSrc: '',
-    desktopSrc: '',
-    mobileSrc: '',
-    maxWidth: 0,
     lazyLoad: false
   };
 
-  constructor(private translate: TranslateService) {}
+  constructor(
+    private translate: TranslateService,
+    private renderer: Renderer2,
+    private el: ElementRef
+  ) {}
+
+  ngAfterViewInit() {
+    this.updateImageSrc();
+    window.addEventListener('resize', () => this.updateImageSrc());
+  }
+
+  updateImageSrc() {
+    const screenWidth = window.innerWidth;
+
+    // Find the matching breakpoint for the current screen width
+    const breakpoint = this.config.breakpoints.find(
+      (breakpoint) => screenWidth <= breakpoint.maxWidth
+    );
+
+    if (breakpoint) {
+      // Set the src attribute of the image to the matched breakpoint's src
+      this.renderer.setAttribute(
+        this.image.nativeElement,
+        'src',
+        breakpoint.src
+      );
+    } else {
+      // If no matching breakpoint is found, use the default src
+      this.renderer.setAttribute(
+        this.image.nativeElement,
+        'src',
+        this.config.defaultSrc
+      );
+    }
+  }
 }
--- component-library/src/app/pages/icon-button-documentation/icon-button-documentation.component.ts
+++ component-library/src/app/pages/icon-button-documentation/icon-button-documentation.component.ts
@@ -44,30 +44,39 @@ export class IconButtonDocumentationComponent implements OnInit {
 
   imageSpacingConfig: IResponsiveImageComponentConfig = {
     id: 'image-spacing',
-    mobileSrc: 'assets/img/icon-btn-content/icon-spacing-phone-328w.png',
-    desktopSrc: 'assets/img/icon-btn-content/icon-spacing.png',
+    breakpoints: [
+      {
+        maxWidth: 768,
+        src: 'assets/img/icon-btn-content/icon-spacing-phone-328w.png'
+      }
+    ],
     defaultSrc: 'assets/img/icon-btn-content/icon-spacing.png',
-    maxWidth: 768,
     altText: 'IconButtonDocumentation.Spacing.ImgAlt',
     lazyLoad: true
   };
 
   anatomyImgConfig: IResponsiveImageComponentConfig = {
     id: 'anatomy-image',
-    mobileSrc: 'assets/img/icon-btn-content/icon-anatomy-phone.png',
-    desktopSrc: 'assets/img/icon-btn-content/anatomy-image-button.png',
+    breakpoints: [
+      {
+        maxWidth: 768,
+        src: 'assets/img/icon-btn-content/icon-anatomy-phone.png'
+      }
+    ],
     defaultSrc: 'assets/img/icon-btn-content/anatomy-image-button.png',
-    maxWidth: 768,
     altText: 'IconButtonDocumentation.AnatomyImgAlt',
     lazyLoad: true
   };
 
   specImgConfig: IResponsiveImageComponentConfig = {
     id: 'spec-image',
-    mobileSrc: 'assets/img/icon-btn-content/icon-spec-phone-328w.png',
-    desktopSrc: 'assets/img/icon-btn-content/spec-image-872w.png',
+    breakpoints: [
+      {
+        maxWidth: 768,
+        src: 'assets/img/icon-btn-content/icon-spec-phone-328w.png'
+      }
+    ],
     defaultSrc: 'assets/img/icon-btn-content/spec-image-872w.png',
-    maxWidth: 768,
     altText: 'IconButtonDocumentation.SpecsImgAlt',
     lazyLoad: true
   };
